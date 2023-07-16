fd9cc68b249481cdf01a21123412f36eb4cee2a0
tienbui <tienbui1508@gmail.com>
2023-07-16 23:05:18 +0930

    all done

--- LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/ContentView.swift
@@ -8,15 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+    let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
+
+        var body: some View {
+            GeometryReader { fullView in
+                ScrollView(.vertical) {
+                    ForEach(0..<50) { index in
+                        GeometryReader { geo in
+                            Text("Row #\(index)")
+                                .font(.title)
+                                .frame(maxWidth: .infinity)
+                                .background(Color(
+                                    hue: min(1 , geo.frame(in: .global).minY / fullView.size.height),
+                                    saturation: 1,
+                                    brightness: 1
+                                ))
+                                .rotation3DEffect(.degrees(geo.frame(in: .global).minY - fullView.size.height / 2) / 5, axis: (x: 0, y: 1, z: 0))
+                                .opacity(geo.frame(in: .global).minY / 200)
+                                .onTapGesture {
+                                    print(1 - Double(59 / geo.frame(in: .global).minY))
+                                }
+                                .scaleEffect(max(0.5, 1 - Double(59 / geo.frame(in: .global).minY)))
+                        }
+                        .frame(height: 40)
+                    }
+                }
+            }
         }
-        .padding()
-    }
 }
 
 struct ContentView_Previews: PreviewProvider {
